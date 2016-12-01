729c177f1e7c4fa675bbaa93f323fcbcc8cd4f1a
zhou_jc <zhou_jc@Ctrip.com>
2016-12-01 19:52:28 +0800

    netty demo

---
+++ src/main/java/NettyDemo/echo/handler/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package NettyDemo.echo.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Sharable表示此对象在channel间共享
+ * handler类是我们的具体业务类
+ * */
+@Sharable//注解@Sharable可以让它在channels间共享
+public class EchoServerHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("server received data :" + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);//写回数据，
+    }
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER) //flush掉所有写回的数据
+                .addListener(ChannelFutureListener.CLOSE); //当flush完成后关闭channel
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause) {
+        cause.printStackTrace();//捕捉异常信息
+        ctx.close();//出现异常时关闭channel
+    }
+}
